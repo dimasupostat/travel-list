3cd13bb97c74831e4c8a345d1f051a1b813e10e7
Dima <dima@Dimas-MacBook-Pro.local>
2025-03-19 21:29:17 +0200

    test

--- src/App.js
+++ src/App.js
@@ -16,37 +16,48 @@ export default function App() {
   );
 }
 function Logo() {
-  return <h1>🌴Far Away💼</h1>;
+  return <h1>🌴Far Away</h1>;
 }
 
+function Form() {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
 
-  function Form(){
-    const [description, setDescription]= useState("");
-    const [quantity, setQuantity]=useState(1)
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (!description) return;
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+    console.log(newItem);
 
-    function handleSubmit(e) {
-      e.preventDefault();
-      if(!description) return;
-      const newItem = {description, quantity, packed:false, id:Date.now()}
-      console.log(newItem)
-      
-      setDescription('');
-      setQuantity(1);
-    }
-    return (
-      <form className="add-form" onSubmit={handleSubmit}>
-        <h3>What do you need for your trip?</h3>
-        <select value={quantity} onChange={(e) => setQuantity(Number(e.target.value))}>
-          {Array.from({length:20}, (_, i)=> i+1).map((num)=>(<option value={num} key={num} >{num}</option>))}
-        </select>
-        <input type="text" placeholder="Item..." value={description} onChange={(e)=> setDescription(e.target.value)}></input>
-        <button>Add</button>
-      </form>
-    )
+    setDescription("");
+    setQuantity(1);
   }
-  function PackingList(){
-    return(
-      <div className="list">
+  return (
+    <form className="add-form" onSubmit={handleSubmit}>
+      <h3>What do you need for your trip?</h3>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      ></input>
+      <button>Add</button>
+    </form>
+  );
+}
+function PackingList() {
+  return (
+    <div className="list">
       <ul>
         {initialItems.map((item) => (
           <Item item={item} key={item.id} />
